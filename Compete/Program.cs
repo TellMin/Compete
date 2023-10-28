d9a1c9b403aa383327ec5b96d743882c0e9b3000
TellMin <dhgirkcls@gmail.com>
2023-10-28 20:39:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Compete/Program.cs
@@ -0,0 +1,12 @@
+﻿using AtCoder;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    static void Main()
+    {
+        SourceExpander.Expander.Expand();
+    }
+}
